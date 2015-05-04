af7bec4caf074585c819909c121895cae00c4d1b
Kris Neal <kisflying@gmail.com>
2015-05-04 13:17:00 +0800

    Remove the page title bar from post list page.

--- wp-content/themes/topshop/page.php
+++ wp-content/themes/topshop/page.php
@@ -24,6 +24,8 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+            
+            <?php get_template_part( '/templates/titlebar' ); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
