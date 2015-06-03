0a4ac361bbe8ec9430cfb0f44978a7e134d750c1
Kris Neal <kisflying@gmail.com>
2015-06-03 11:28:05 +0800

    Update theme

--- wp-content/plugins/woocommerce/templates/content-product.php
+++ wp-content/plugins/woocommerce/templates/content-product.php
@@ -33,14 +33,6 @@ if ( ! $product || ! $product->is_visible() ) {
 // Increase loop count
 $woocommerce_loop['loop']++;
 
-// Extra post classes
-$classes = array();
-if ( 0 == ( $woocommerce_loop['loop'] - 1 ) % $woocommerce_loop['columns'] || 1 == $woocommerce_loop['columns'] ) {
-	$classes[] = 'first';
-}
-if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] ) {
-	$classes[] = 'last';
-}
 ?>
 <li <?php post_class( $classes ); ?>>
 
--- wp-content/plugins/woocommerce/templates/loop/result-count.php
+++ wp-content/plugins/woocommerce/templates/loop/result-count.php
@@ -15,6 +15,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $wp_query;
 
+return;
+
 if ( ! woocommerce_products_will_display() )
 	return;
 ?>
