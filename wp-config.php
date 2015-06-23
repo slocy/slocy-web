57b485c65f62a5a8b8f33f0ec7b585b28507ba6a
James Chen <JAMES@JAMES-MacBook-Pro.local>
2015-06-23 20:44:14 +0800

    SSL Insecure Content Fixer

--- wp-config.php
+++ wp-config.php
@@ -76,6 +76,10 @@ define('WP_DEBUG', false);
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
+/** Our server is behind a load balancer or reverse proxy, so we need this snippet. */
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+    $_SERVER['HTTPS']='on';
+
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
 $plugins = get_option( 'active_plugins' );
