5bd31d29be83d6a378986698bae3d5cebf86e675
slocy <git@slocy.com>
2015-06-28 14:56:09 +0000

    wordpress update

--- wp-content/themes/big-pix/library/js/scripts-home.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function() {
-			var jQueryslider = jQuery('.cycle-slideshow');
-			jQueryslider.imagesLoaded( function() {
-			jQuery('#load-cycle').hide(); /* preloader */
-			jQueryslider.slideDown(1000);
-			});
-		});
